148f578bcdd02c88ba363bf8fe4eb5c354341148
sam <s.gelernter@gmail.com>
2021-11-04 17:25:56 -0400

    Add random code method

--- url_shortener/URLShortener/app/models/shortened_url.rb
+++ url_shortener/URLShortener/app/models/shortened_url.rb
@@ -1,6 +1,16 @@
 
 
 class ShortenedUrl < ApplicationRecord
+
+    def self.random_code
+        shortened = SecureRandom.urlsafe_base64
+        until !self.exists?(['short_url = ?', shortened]) 
+            shortened = SecureRandom.urlsafe_base64
+        end
+        self.short_url = shortened   
+    end
+
+
     validates :long_url, :user_id, presence: true 
     validates :long_url, uniqueness: true 
 
