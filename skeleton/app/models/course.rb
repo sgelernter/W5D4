0360203e8585b5784531dbff10a90c0655f083e6
Anuj Gupta <apgupta3091@gmail.com>
2021-11-04 15:23:21 -0400

    finished associations

--- skeleton/app/models/course.rb
+++ skeleton/app/models/course.rb
@@ -10,4 +10,25 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+
+    has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+
+    has_one :prerequisite,
+    primary_key: :prereq_id,
+    foreign_key: :id,
+    class_name: :Course
+
+
+    has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
+
+    belongs_to :instructor,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :User
+
 end
